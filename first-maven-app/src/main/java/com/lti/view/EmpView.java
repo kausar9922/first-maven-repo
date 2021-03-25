1c279a575f90168f354bb86c0b7711e15b7025c1
Kausar <shaikhkausar9922@gmail.com>
2021-03-25 12:44:08 +0530

    changed name and ID

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,8 +6,8 @@ public class EmpView {
 
 		public static void main(String[] args){
 			Employee emp=new Employee();
-			emp.setEmpId(1002);
-			emp.setEmpName("Kausar");
+			emp.setEmpId(102);
+			emp.setEmpName("KausarShaikh");
 			
 			System.out.println("Id" +emp.getEmpId());
 			System.out.println("Name:" +emp.getEmpName());
