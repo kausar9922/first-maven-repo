fc5af9e78ca349907e1484f4229ae568c401a2fb
Kausar <shaikhkausar9922@gmail.com>
2021-03-25 10:01:09 +0530

    first commit

---
+++ first-maven-app/src/main/java/com/lti/model/Employee.java
@@ -0,0 +1,20 @@
+package com.lti.model;
+
+public class Employee {
+	int EmpId;
+	String empName;
+	public int getEmpId() {
+		return EmpId;
+	}
+	public void setEmpId(int empId) {
+		EmpId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+}
